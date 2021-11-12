21fca59753510c9dcf28d7459a3ab094779a5088
Jorge Osses <jorgitoosses@gmail.com>
2021-11-12 13:15:48 -0300

    form style

---
+++ js/contacto.js
@@ -0,0 +1,29 @@
+var form = document.getElementById("my-form");
+const divMensaje = document.createElement('p');
+divMensaje.classList.add('success', 'hidden');
+divMensaje.setAttribute("id", "my-form-status");
+
+        async function handleSubmit(event) {
+            event.preventDefault();
+            document.querySelector('.resetContact').insertBefore(divMensaje, null);
+            
+            var status = document.getElementById("my-form-status");
+            var data = new FormData(event.target);
+            fetch(event.target.action, {
+                method: form.method,
+                body: data,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(response => {
+                status.classList.remove('hidden');
+                status.innerHTML = "Gracias por comunicarse!";
+                form.reset()
+                setTimeout(() => {
+                    status.remove();
+                },4000)
+            }).catch(error => {
+                status.innerHTML = "Uups! Hay un problema para enviarlo"
+            });
+        }
+        form.addEventListener("submit", handleSubmit)
